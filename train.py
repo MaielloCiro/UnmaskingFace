8eed5b20b85d7e5614ef896cbfbb46702d9428bd
MaielloCiro <ciro.maiello@studenti.polito.it>
2021-03-27 12:51:47 +0100

    Update train.py
    Creato grafico accuracy, loss e messa batch_size che funziona su colab ma su anaconda no. (Vedere perché)

--- train.py
+++ train.py
@@ -1,9 +1,18 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from prepare_dataset import *
 from map_module_model import *
 
 model = modello()
 x_train, x_gt = prepare()
 print("Preparati i dati")
-model.fit(x_train, x_gt, epochs=100)
+history = model.fit(x_train, x_gt, epochs=100, batch_size=32)
 model.save('prova.h5')
+
+print(history.history.keys())
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['loss'])
+plt.title('accuracy and loss')
+plt.xlabel('epoch')
+plt.legend(['accuracy', 'loss'], loc='upper left')
+plt.show()
